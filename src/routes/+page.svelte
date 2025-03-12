39a9d49737723f5a63517d8e66bb7728814ba305
Tamer Ramzy <tamerihab@gmail.com>
2025-03-13 02:37:05 +0400

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<div class="p-8">
+	<div class="mb-4 font-bold">Skeleton Demo</div>
+	<select class="select w-64">
+		<option value="">Select an option</option>
+		<option value="option1">Option 1</option>
+	</select>
+</div>
